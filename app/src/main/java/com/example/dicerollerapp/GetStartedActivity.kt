d303c0ac7da54829885a245a4bb132ca16efc2bd
M.Hamed <mhamed@victorylink.com>
2025-08-17 13:34:53 +0300

    Make status bar icons light in GetStartedActivity
    This commit changes the status bar icon color to light in `GetStartedActivity` by setting `isAppearanceLightStatusBars` to `false` using `WindowCompat`.

--- app/src/main/java/com/example/dicerollerapp/GetStartedActivity.kt
+++ app/src/main/java/com/example/dicerollerapp/GetStartedActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 
 class GetStartedActivity : AppCompatActivity() {
@@ -16,6 +17,9 @@ class GetStartedActivity : AppCompatActivity() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            WindowCompat.getInsetsController(window, window.decorView).apply {
+                isAppearanceLightStatusBars = false
+            }
             insets
         }
 
